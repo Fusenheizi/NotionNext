f57ea7530bb564bac6d3366b7d6f28efbc70bc78
Fusenheizi <fusenheizi@qq.com>
2025-04-06 19:03:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '浮森黑子', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '"每日都是新的，此事明日说吧。"', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://interwink.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '脑机接口, 神经调控, 脑电解码, 信号分析', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
